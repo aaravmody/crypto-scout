506bcdca7c0d8a10df05566af1d8c69edf882ab2
aaravmody <aaravmody2@gmail.com>
2024-04-10 13:13:57 +0530

    crypo

---
+++ src/cryptoContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+
+
+const crypto = createContext ()
+
+const cryptoContext = ({children}) => {
+
+    const [currency,setCurrency] = useState("INR")
+    const [symbol,setSymbol] = useState("₹");
+
+    useEffect(()=>{
+        if(currency==="INR") setSymbol('₹');
+        else if(currency==="USD") setSymbol('$');
+    },[currency]);
+  return (
+    <Crypto.Provider value={{currency,symbol,setCurrency}}>
+        {children}
+        </Crypto.Provider>
+  )
+}
+
+export default cryptoContext
+
+export const cryptoState = () =>{
+    return useContext(crypto);
+}
